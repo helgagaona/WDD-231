afcb7d3e453d43ae759a5b101ad1fea94c653695
helgagaona <gao20003@byui.edu>
2026-02-06 20:44:46 -0700

    nps 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,6 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
-
-function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-banner__title">${info.name}</a>
-    <p class="hero-banner__subtitle">
-        <span>${info.designation}</span>
-        <span>${info.states}</span>
-    </p>`
-}
-
-function setHeaderInfo(data) {
-  // insert data into disclaimer section
-  const disclaimer = document.querySelector(".disclaimer > a");
-  disclaimer.href = data.url;
-  disclaimer.innerHTML = data.fullName;
-  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-  document.querySelector("head > title").textContent = data.fullName;
-  // set the banner image
-  document.querySelector(".hero-banner > img").src = data.images[0].url;
-  // use the template function above to set the rest of the park specific info in the header
-  document.querySelector(".hero-banner__content").innerHTML =
-    parkInfoTemplate(data);
-}
 
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
@@ -40,7 +18,7 @@ function setParkInfoLinks(data) {
 
 async function init() {
   const parkData = await getParkData();
-
+  const links = getInfoLinks(parkData.images);
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(links);
